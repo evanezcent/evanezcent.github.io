ee31c8540d234d644f2079eea898c40f59da3c59
pratamays <pratamayo16@gmail.com>
2019-03-17 21:58:01 +0700

    add skill, but progress bar doesn't work

---
+++ js/loadSkill.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    var skil = (data) => {
+        return '<span class="label_progress font-weight-bold">' + data.nama + '</span>' +
+            '<div class="progress"'+
+            '<div class="progress-bar bg-dark" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="'+data.persen+'" style="width:'+data.persen+'%">' +
+             + data.persen +
+            '</div>' +
+            '</div>';
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "js/skills.json",
+        dataType : "json",
+        success : function(skillData){
+            $.each(skillData.skills, function(index, obj){
+                $("#mySkills").append(skil(obj));
+            });
+        }
+    });
+});
