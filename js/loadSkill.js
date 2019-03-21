29d44d1cc01018d8d79ed5d09d024fcbdba0d7d4
pratamays <pratamayo16@gmail.com>
2019-03-22 00:23:39 +0700

    Progress Bar now is work, add container

--- js/loadSkill.js
+++ js/loadSkill.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
 
     var skil = (data) => {
         return '<span class="label_progress font-weight-bold">' + data.nama + '</span>' +
-            '<div class="progress"'+
-            '<div class="progress-bar bg-dark" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="'+data.persen+'" style="width:'+data.persen+'%">' +
+            '<div class="progress">'+
+            '<div class="progress-bar bg-dark progress-bar-striped progress-bar-animated" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="'+data.persen+'" style="width:'+data.persen+'%">' +
              + data.persen +
-            '</div>' +
+            '%</div>' +
             '</div>';
     }
 
